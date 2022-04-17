a1f28e458f54ec07e36470c9e05d9c62fb1c16eb
mahsa-nvr <mahsaanvari2@gmail.com>
2022-04-17 18:33:32 +0430

    install redux & add user reducer & root reducer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider>
       <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
