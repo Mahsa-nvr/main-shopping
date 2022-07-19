4a1bcb064c72f97355daf08b6a76501782a32523
mahsa-nvr <mahsaanvari2@gmail.com>
2022-07-19 07:02:41 +0430

    fix state normalization

--- src/pages/collectionpage/collectionpage.component.js
+++ src/pages/collectionpage/collectionpage.component.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
-
+import './collection.styles.css';
 import { selectShopItems } from '../../redux/shop/shop.selector';
-
+import CollectionItem from '../../components/collection-item/collection-item.component'
 const CollectionPage = ({collections}) => {
     let params = useParams()
-    const test = collections.find(collect => collect.routeName === params.collection)
-    console.log('yes',test)
+    const collection = collections.find(collect => collect.routeName === params.collection)
+    const { title, items } = collection;
     return (
-        <div>collection page</div>
+        <div className='collection-page'>
+           <h2 className='title'>{title}</h2>
+           <div className='items'>
+           {
+            items.map(item => <CollectionItem key={item.id} item={item}/>)
+           }
+           </div>
+        </div>
     )
 }
 
